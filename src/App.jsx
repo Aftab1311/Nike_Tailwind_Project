05b257413b56cc98ccc36c37d769a8c24876b845
abc <abc@example.com>
2023-11-25 15:11:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App =() => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+     Hero
+    </section>
+     <section className="padding">
+      Popular Products Section
+     </section>
+     <section className="padding">
+      superquality
+     </section>
+     <section className="padding">
+      Services
+     </section>
+     <section className="padding-x py-10">
+      Special Offers
+     </section>
+     <section className="bg-pale-blue padding">
+      Customer Reviews
+     </section>
+     <section className="padding-x py-10">
+      Subscribe 
+     </section>
+     <section className="padding-x py-10">
+      Footer
+     </section>
+  </main>
+)
+
+export default App
